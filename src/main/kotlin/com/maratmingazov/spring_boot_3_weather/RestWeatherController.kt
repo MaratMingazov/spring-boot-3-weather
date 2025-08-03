46c6abef0f3c68b2d7965e0e8d945b60cc7372b4
MaratMingazov <marat@miro.com>
2025-08-04 00:15:17 +0400

    Code refactor

--- src/main/kotlin/com/maratmingazov/spring_boot_3_weather/RestWeatherController.kt
+++ src/main/kotlin/com/maratmingazov/spring_boot_3_weather/RestWeatherController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class WeatherController(
+class RestWeatherController(
     private val weatherService: WeatherService
 ) {
 
